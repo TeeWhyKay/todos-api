b6b59c4b045330cf5e6a52686f7684eaf67fa9e3
YongKeeTan <yongkee.t@gmail.com>
2021-10-16 17:59:42 +0800

    update routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :todos, only: [:index, :show, :create, :update, :destroy] do
+    resources :items, only: [:index, :show, :create, :update, :destroy]
+  end
 end
