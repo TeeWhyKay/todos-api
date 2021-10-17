5a9bc0817bed776dcd2e38d2cfdbfbec84d12746
YongKeeTan <yongkee.t@gmail.com>
2021-10-17 22:51:30 +0800

    test suite for items index

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  let!(:todo) { create(:todo) }
+  let!(:items) { create_list(:item, 20, todo_id: todo.id) }
+  let(:todo_id) { todo.id }
+  let(:id) { items.first.id }
+
+  # Test suite for GET /todos/:todo_id/items
+  describe 'GET /todos/:todo_id/items' do
+    before { get "/todos/#{todo.id}/items" }
+
+    context 'when todo exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all todo items' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when todo does not exist' do
+      it 'returns status code 404' do
+        expect(response).to m
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
 
 end
