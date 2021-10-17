113e496f9d57eaa485909cadd7fc951bf2e31185
YongKeeTan <yongkee.t@gmail.com>
2021-10-17 21:55:56 +0800

    create action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,15 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+  # GET /todos
   def index
     @todos = Todo.all
     json_response(@todos)
   end
+
+  # POST /todos
+  def
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
 end
