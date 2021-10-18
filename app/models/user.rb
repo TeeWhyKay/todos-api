925d6b7a5cd7b15aaabc43ce0051403eaaf53327
YongKeeTan <yongkee.t@gmail.com>
2021-10-18 17:18:04 +0800

    user model associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # encrypt password how?
+  has_secure_password
+
+  # Model associations
+  has_many :todos, foreign_key: :created_by
+  validates :name, :email, :password_digest, presence: true
 end
